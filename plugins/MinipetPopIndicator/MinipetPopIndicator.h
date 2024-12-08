6edb6cc269ec9800c6ab1681132153341cc8cf6f
Jabor <lja@posteo.de>
2024-12-08 01:34:55 +0100

    Minipet pop indicator plugin

---
+++ plugins/MinipetPopIndicator/MinipetPopIndicator.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <ToolboxUIPlugin.h>
+#include <imgui.h>
+
+class MinipetPopIndicator : public ToolboxUIPlugin {
+public:
+    MinipetPopIndicator();
+    ~MinipetPopIndicator() override = default;
+
+    const char* Name() const override { return "MinipetPopIndicator"; }
+    const char* Icon() const override { return ICON_FA_CIRCLE; }
+
+    void LoadSettings(const wchar_t*) override;
+    void SaveSettings(const wchar_t*) override;
+    void DrawSettings() override;
+    void Draw(IDirect3DDevice9* pDevice) override;
+
+    void Initialize(ImGuiContext* ctx, ImGuiAllocFns allocator_fns, HMODULE toolbox_dll) override;
+    void Terminate() override;
+
+private:
+    float radius = 50.f;
+    ImVec4 color = {0.247f, 0.282f, 0.8f, 1.f};
+};
