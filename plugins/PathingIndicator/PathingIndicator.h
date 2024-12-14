9aaa449c7b4c6f60c97db565ee52dc5dad6da5df
Jabor <lja@posteo.de>
2024-12-15 00:07:23 +0100

    WIP pathingindicator finds function

--- plugins/PathingIndicator/PathingIndicator.h
+++ plugins/PathingIndicator/PathingIndicator.h
@@ -2,6 +2,22 @@
 
 #include <ToolboxUIPlugin.h>
 
+#include <GWCA/Constants/Skills.h>
+#include <GWCA/GameEntities/Pathing.h>
+
+#include <unordered_map>
+
+struct OutcomeChances {
+    float success = 0.f;
+    float partial = 0.f;
+    float failure = 0.f;
+};
+
+struct PathPoint {
+    GW::GamePos pos = {};
+    const GW::PathingTrapezoid* t = nullptr;
+};
+
 class PathingIndicator : public ToolboxUIPlugin {
 public:
     PathingIndicator()
@@ -15,12 +31,21 @@ public:
 
     const char* Name() const override { return "Pathing Indicator"; }
 
+    void Draw(IDirect3DDevice9* pDevice) override;
+    void Update(float) override;
+
     void LoadSettings(const wchar_t*) override;
     void SaveSettings(const wchar_t*) override;
     void DrawSettings() override;
+
     void Initialize(ImGuiContext* ctx, ImGuiAllocFns allocator_fns, HMODULE toolbox_dll) override;
     void SignalTerminate() override;
     bool CanTerminate() override;
-    // Draw user interface. Will be called every frame if the element is visible
-    void Draw(IDirect3DDevice9* pDevice) override;
+    void Terminate() override;
+
+private:
+    std::unordered_map<GW::Constants::SkillID, OutcomeChances> chances;
+    PathPoint shadowOfHasteLocation{};
+    PathPoint shadowWalkLocation{};
+    bool skillbar_position_dirty = true;
 };
