fa799a212b691959d964e0ae76384bd555ffab8e
Jabor <lja@posteo.de>
2025-12-06 14:41:42 +0100

    Bump versions

--- plugins/SpeedrunScriptingTools/SpeedrunScriptingTools.cpp
+++ plugins/SpeedrunScriptingTools/SpeedrunScriptingTools.cpp
@@ -750,7 +750,7 @@ void SpeedrunScriptingTools::DrawSettings()
     ImGui::SameLine();
     ImGui::Checkbox("Block hotkey keys even if conditions not met", &alwaysBlockHotkeyKeys);
 
-    ImGui::Text("Version 2.5. For new releases, feature requests and bug reports check out");
+    ImGui::Text("Version 2.5.1 For new releases, feature requests and bug reports check out");
     ImGui::SameLine();
 
     constexpr auto discordInviteLink = "https://discord.gg/ZpKzer4dK9";
